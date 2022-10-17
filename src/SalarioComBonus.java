c12fd9841017f5fcb53f878baacb25153533c043
Richard Leonardo <paulino_richard@hotmail.com>
2022-10-17 20:21:33 -0300

    Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas.

---
+++ src/SalarioComBonus.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SalarioComBonus {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        String nome = input.nextLine();
+        double salario = input.nextDouble();
+        double totalVendas = input.nextDouble();
+
+        double totalDeComissao = salario + (totalVendas * 0.15);
+        System.out.printf("TOTAL = R$ %.2f\n", totalDeComissao);
+
+    }
+}
