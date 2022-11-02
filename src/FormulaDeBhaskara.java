f4177442b988ce2ea86c51ad77403164999e7776
Richard Leonardo <paulino_richard@hotmail.com>
2022-11-01 22:17:37 -0300

    Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.

--- src/FormulaDeBhaskara.java
+++ src/FormulaDeBhaskara.java
@@ -13,14 +13,17 @@ public class FormulaDeBhaskara {
         B = input.nextDouble();
         C = input.nextDouble();
 
+        //Delta = b² - 4 a c
         double delta = Math.pow(B, 2) - (4 * A * C);
+        //Bhaskara = (-b ± √∆) / 2.a
         double R1 = (- B + Math.sqrt(delta)) / (2 * A);
         double R2 = (- B - Math.sqrt(delta)) / (2 * A);
 
         if (delta > 0  &&  A != 0){
             System.out.printf("R1 = %.5f\n", R1);
             System.out.printf("R2 = %.5f\n", R2);
-        }else {
+        }
+        else {
             System.out.println("Impossivel calcular");
         }
 
